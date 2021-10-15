420753b39e1f30ba243441de9c5a75e458aae0e9
unknown <gagahre4@gmail.com>
2021-10-15 14:39:06 +0300

    Added Task1 Folder

---
+++ Task1/Task1/DBLayer/Model/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+using System.ComponentModel;
+
+namespace Task1.DBLayer.Model
+{
+
+
+    [Serializable]
+    public partial class Person
+    {
+        public Person()
+        {
+            
+        }
+        
+        [JsonProperty("id")]
+        public int PkPersonId { get; set; }
+        [JsonProperty("name")]
+        public string PersonName { get; set; }
+        [JsonIgnore]
+        public int? FkParentPersonId { get; set; }
+        [JsonIgnore]
+        public  Person FkParentPerson { get; set; }
+
+        [JsonProperty("child")]
+        public List<Person> InverseFkParentPerson { get; set; }
+
+        public bool ShouldSerializeInverseFkParentPerson()
+        {
+            return (InverseFkParentPerson.Count != 0);
+        }
+    }
+}
